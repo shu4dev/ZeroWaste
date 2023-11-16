8928d6b6cbf46842c2f4b1932ba8275d55358a62
Brian Shu <110252035+shu4dev@users.noreply.github.com>
2023-11-15 23:22:32 -1000

    deploy

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-require('dotenv').config();
 
 const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
