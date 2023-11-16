a3d2a82f5ce3da1b63a8f1ce6b6a2edded2bd6d5
Brian Shu <110252035+shu4dev@users.noreply.github.com>
2023-11-15 23:27:33 -1000

    update gitingore

--- api/index.js
+++ api/index.js
@@ -17,6 +17,7 @@ app.use(cors({
   optionSuccessStatus:200,
 }))
 app.use(express.json());
+
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
