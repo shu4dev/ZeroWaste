a9899bd2e9bb1e15c5028135d2138f13067f3d59
shu4dev <shu4@hawaii.edu>
2023-11-15 15:02:51 -1000

    update backend

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const HID = require('node-hid');
+const cors = require('cors');
 require('dotenv').config();
 
 const mongoString = process.env.DATABASE_URL
