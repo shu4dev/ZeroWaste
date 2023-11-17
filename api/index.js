91c639802c2892bcd99cb42cd3867b7794556120
Brian Shu <110252035+shu4dev@users.noreply.github.com>
2023-11-16 22:51:11 -1000

    redo

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const HID = require('node-hid');
 const cors = require('cors');
 require('dotenv').config();
+
 const mongoString = process.env.DATABASE_URL
 mongoose.connect(mongoString, { dbName: 'main'});
+
+const port = 4000;
 const database = mongoose.connection
-
 const app = express();
-
-const port = process.env.PORT;
-
-app.use(express.json());
-
 app.use(cors({
   origin:'*', 
   credentials:true,
   optionSuccessStatus:200,
 }))
+app.use(express.json());
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,14 +24,25 @@ app.all('/', function(req, res, next) {
   next();
  });
 
- app.get('/', (req, res) =>{
-
-  res.send("Hello World");
+app.use('/api', routes)
+app.get('/', (req, res) =>{
+  res.send("Hello World")
 })
 
-app.listen(port, () => {
-  console.log(`Server Started at ${port}`)
-})
+app.get('/checkDevice', (req, res) => {
+  try {
+    const device = new HID.HID(16701, 8455);
+    console.log("device connect")
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  }
+});
+
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); 
+}); 
+
 
 database.on('error', (error) => {
   console.log(error)
@@ -41,6 +52,9 @@ database.once('connected', () => {
   console.log('Database Connected');
 })
 
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`)
+})
 /**
  * 
 
